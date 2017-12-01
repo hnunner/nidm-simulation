d1c24d63a159cba2b9662a49f67520e81c5d8ee3
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2017-12-01 17:38:18 +0100

    stats showing network stability
    Former-commit-id: 465752e08b7df1cbc0a88d974df0e292fa8bc00b

--- src/main/java/nl/uu/socnetid/network_games/network/networks/Network.java
+++ src/main/java/nl/uu/socnetid/network_games/network/networks/Network.java
@@ -62,4 +62,33 @@ public interface Network {
      */
     void toggleInfection(long playerId, Disease disease);
 
+    /**
+     * Sets whether the network is stable or not.
+     *
+     * @param stable
+     *          flag whether the network is stable or not
+     */
+    void setStable(boolean stable);
+
+    /**
+     * @return true if no player wants to add or remove connections, false otherwise
+     */
+    boolean isStable();
+
+    /**
+     * Adds a listener to be notified when network stability changes.
+     *
+     * @param listener
+     *          the listener to be notified
+     */
+    void addListener(final NetworkStabilityListener listener);
+
+    /**
+     * Removes a listener.
+     *
+     * @param listener
+     *          the listener to be removed
+     */
+    void removeListener(final NetworkStabilityListener listener);
+
 }
