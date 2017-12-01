8c1b259447c698f360b38cf559f752c48a772e21
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2017-12-01 17:38:18 +0100

    stats showing network stability
    Former-commit-id: 465752e08b7df1cbc0a88d974df0e292fa8bc00b

---
+++ src/main/java/nl/uu/socnetid/network_games/network/networks/NetworkStabilityListener.java
@@ -0,0 +1,17 @@
+package nl.uu.socnetid.network_games.network.networks;
+
+/**
+ *
+ * @author Hendrik Nunner
+ */
+public interface NetworkStabilityListener {
+
+    /**
+     * Entry point for network stability notifications.
+     *
+     * @param network
+     *          the network
+     */
+    void notify(final Network network);
+
+}
