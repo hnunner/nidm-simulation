179e9a7fd38f176fd5f907f694249871d802eb33
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2018-02-23 11:28:06 +0100

    some experiments with graphstream's GEXFFileSink
    Former-commit-id: 1a0b2be47c873a32eda6de3aca73440df371c036

---
+++ src/main/java/nl/uu/socnetid/networkgames/network/io/GEXFWriter.java
@@ -0,0 +1,14 @@
+package nl.uu.socnetid.networkgames.network.io;
+
+/**
+ * @author Hendrik Nunner
+ */
+public class GEXFWriter {
+
+    // TODO implement
+    // TODO extend graphstream GEXFWriter
+    // TODO have own counter that increases every time a method is called and the simulation is running
+    // TODO implement listeners that are called each time the network structure changes, preferrably at a
+    //      central point such as the network or the network simulation itself
+
+}
