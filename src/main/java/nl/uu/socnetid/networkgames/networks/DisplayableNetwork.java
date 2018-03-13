adcca205ea33a93a46a17c8789b5b1d59a6c6455
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2018-03-13 17:27:53 +0100

    bugfix for SpriteManager; now it's part of the DisplayableNetwork, not the normal network
    Former-commit-id: 6857613ec48993a754e4f6e73f56cae64c85b911

--- src/main/java/nl/uu/socnetid/networkgames/networks/DisplayableNetwork.java
+++ src/main/java/nl/uu/socnetid/networkgames/networks/DisplayableNetwork.java
@@ -3,7 +3,14 @@ package nl.uu.socnetid.networkgames.networks;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
+import org.graphstream.ui.graphicGraph.stylesheet.StyleConstants.Units;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
 import org.graphstream.ui.view.Viewer;
+
+import nl.uu.socnetid.networkgames.actors.Actor;
+import nl.uu.socnetid.networkgames.diseases.DiseaseSpecs;
+import nl.uu.socnetid.networkgames.utilities.UtilityFunction;
 
 /**
  * @author Hendrik Nunner
@@ -16,6 +23,9 @@ public class DisplayableNetwork extends Network {
 
     // graphstream
     private Viewer viewer;
+
+    // sprite manager
+    private SpriteManager spriteManager;
 
 
     /**
@@ -30,6 +40,37 @@ public class DisplayableNetwork extends Network {
         URL gsStyles = this.getClass().getClassLoader().getResource("graph-stream.css");
         this.addAttribute("ui.stylesheet", "url('file:" + gsStyles.getPath() + "')");
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+        // sprite manager
+        this.spriteManager = new SpriteManager(this);
+    }
+
+    /* (non-Javadoc)
+     * @see nl.uu.socnetid.networkgames.networks.Network#addActor(
+     * nl.uu.socnetid.networkgames.utilities.UtilityFunction, nl.uu.socnetid.networkgames.diseases.DiseaseSpecs,
+     * double)
+     */
+    @Override
+    public Actor addActor(UtilityFunction utilityFunction, DiseaseSpecs diseaseSpecs, double riskFactor) {
+        Actor actor = super.addActor(utilityFunction, diseaseSpecs, riskFactor);
+
+        // sprite
+        Sprite sprite = this.spriteManager.addSprite(actor.getId());
+        sprite.setPosition(Units.PX, 15, 0, -90);
+        sprite.attachToNode(actor.getId());
+        sprite.setAttribute("label", actor.getLabel());
+
+        return actor;
+    }
+
+    /* (non-Javadoc)
+     * @see nl.uu.socnetid.networkgames.networks.Network#removeActor()
+     */
+    @Override
+    public String removeActor() {
+        String actorId = super.removeActor();
+        this.spriteManager.removeSprite(actorId);
+        return actorId;
     }
 
 
