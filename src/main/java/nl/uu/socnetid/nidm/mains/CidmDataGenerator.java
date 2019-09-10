cebb1dc18353051634f48f3bf71dc3cc8c46f383
Hendrik Nunner <h.nunner@gmail.com>
2019-09-10 18:37:35 +0200

    removed unused import
    Former-commit-id: 263574b8f1914e104e6d1934188415fdd93d7639

--- src/main/java/nl/uu/socnetid/nidm/mains/CidmDataGenerator.java
+++ src/main/java/nl/uu/socnetid/nidm/mains/CidmDataGenerator.java
@@ -30,7 +30,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
