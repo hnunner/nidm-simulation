5a5be35049ec7d690b13df93ab36a716c2c5afe8
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2019-09-10 15:56:34 +0200

    added copyright statements to all files and outputs
    Former-commit-id: 22c835dedd9d0b2cdb144b838fc2b609a9fa299d

--- src/main/java/nl/uu/socnetid/nidm/agents/Agent.java
+++ src/main/java/nl/uu/socnetid/nidm/agents/Agent.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2017 - 2019
+ *      Hendrik Nunner    <h.nunner@gmail.com>
+ *
+ * This file is part of the NIDM-Simulation project <https://github.com/hnunner/NIDM-simulation>.
+ *
+ * This project is a stand-alone Java program of the Networking during Infectious Diseases Model
+ * (NIDM; Nunner, Buskens, & Kretzschmar, 2019) to simulate the dynamic interplay of social network
+ * formation and infectious diseases.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * References:
+ *      Nunner, H., Buskens, V., & Kretzschmar, M. (2019). A model for the co-evolution of dynamic
+ *      social networks and infectious diseases. Manuscript sumbitted for publication.
+ */
 package nl.uu.socnetid.nidm.agents;
 
 import java.util.ArrayList;
