4368cfe6d386c0333bea5674ce2bd465dea59f22
Hendrik Nunner <h.nunner@gmail.com>
2019-12-05 08:49:35 +0100

    added parameter for shuffling of assortatively selected co-agents

--- src/main/java/nl/uu/socnetid/nidm/agents/AgentAttributes.java
+++ src/main/java/nl/uu/socnetid/nidm/agents/AgentAttributes.java
@@ -41,6 +41,7 @@ public enum AgentAttributes {
     RISK_MEANING_PI("risk.meaning.pi"),
     PHI("phi"),
     OMEGA("omega"),
+    OMEGA_SHUFFLE("omega.shuffle"),
     SATISFIED("satisfied"),
     CONNECTION_STATS("connection.stats");
 
