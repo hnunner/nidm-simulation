c7c0b19ef2999eea7fcf81399e8530b9a5693d18
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2020-04-02 21:59:39 +0200

    removed extensive use of DijkstraShortestPath algorithm to compute close distances

--- src/main/java/nl/uu/socnetid/nidm/stats/StatsComputer.java
+++ src/main/java/nl/uu/socnetid/nidm/stats/StatsComputer.java
@@ -266,45 +266,103 @@ public final class StatsComputer {
     }
 
     /**
-     * Computes the stats for a single agent's connections with a potentially new connection and without an existing connection.
+     * Computes the geographic distance between an agent and all direct connections and returns it grouped by disease states.
      *
      * @param agent
-     *          the agent
-     * @param with
-     *          the potentially new connection
-     * @param without
-     *          the existing connection not to consider
-     * @return the stats for a single agent's connections.
+     *          the agent to get all geographic distances to
+     * @return the geographic distance between an agent and all direct connections and returns it grouped by disease states
      */
-    public static LocalAgentConnectionsStats computeLocalAgentConnectionsStats(Agent agent, Agent with, Agent without) {
-
-        // initialize maps of agents by disease group and distance
-        Map<DiseaseGroup, Map<Integer, Integer>> consByDiseaseGroupAtGeodesicDistance =
-                new HashMap<DiseaseGroup, Map<Integer, Integer>>();
+    public static Map<DiseaseGroup, Map<Double, Integer>> computeDirectConnectionsByDiseaseGroupAtGeographicDistance(Agent agent) {
         Map<DiseaseGroup, Map<Double, Integer>> directConsByDiseaseGroupAtGeographicDistance =
                 new HashMap<DiseaseGroup, Map<Double, Integer>>();
+
         for (DiseaseGroup dg : DiseaseGroup.values()) {
-            consByDiseaseGroupAtGeodesicDistance.put(dg, new HashMap<Integer, Integer>());
             directConsByDiseaseGroupAtGeographicDistance.put(dg, new HashMap<Double, Integer>());
         }
 
-        // initialize list of direct connections
-        List<Agent> directConnections = new LinkedList<Agent>();
+        Iterator<Agent> it = agent.getNetwork().getAgentIterator();
+        while (it.hasNext()) {
+            Agent otherAgent = it.next();
 
-        // fill map of agents by disease group and distance
+            if (agent.isDirectlyConnectedTo(otherAgent)) {
+                // geographic distance to direct connections
+                double ggd = agent.getGeographicDistanceTo(otherAgent);
+                // store disease group and geographic distance
+                switch (otherAgent.getDiseaseGroup()) {
+                    case SUSCEPTIBLE:
+                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).put(ggd,
+                                directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).get(ggd) != null ?
+                                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).get(ggd) + 1 : 1);
+                        break;
+
+                    case INFECTED:
+                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).put(ggd,
+                                directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).get(ggd) != null ?
+                                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).get(ggd) + 1 : 1);
+                        break;
+
+                    case RECOVERED:
+                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).put(ggd,
+                                directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).get(ggd) != null ?
+                                        directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).get(ggd) + 1 : 1);
+                        break;
+
+                    default:
+                        logger.warn("Unhandled disease group: " + otherAgent.getDiseaseGroup());
+                }
+            }
+        }
+        return directConsByDiseaseGroupAtGeographicDistance;
+    }
+
+    /**
+     * Gets the number of direct connections by geographic distance (irrespective of disease group):
+     * map<distance, number>.
+     *
+     * @param agent
+     *          the agent to get the geographic distances for
+     * @return the number of direct connections by disease group and distance
+     */
+    public static Map<Double, Integer> getDirectConnectionsByGeographicDistance(Agent agent) {
+
+        Map<Double, Integer> consByDist = new HashMap<Double, Integer>();
+
+        for (DiseaseGroup dg : DiseaseGroup.values()) {
+             for (Map.Entry<Double, Integer> entry : computeDirectConnectionsByDiseaseGroupAtGeographicDistance(agent).
+                     get(dg).entrySet()) {
+                 Double dist = entry.getKey();
+                 Integer cons = entry.getValue();
+                 consByDist.put(dist,
+                         consByDist.get(dist) != null ?
+                                 consByDist.get(dist) + cons :
+                                     cons);
+             }
+        }
+        return consByDist;
+    }
+
+    /**
+     * Computes the geodesic distance between an agent and all other agents and returns it grouped by disease states.
+     *
+     * @param agent
+     *          the agent to get geodesic distances to
+     * @return the geodesic distance between an agent and all other agents and returns it grouped by disease states
+     */
+    public static Map<DiseaseGroup, Map<Integer, Integer>> computeConnectionsByDiseaseGroupAtGeodesicDistance(Agent agent) {
+        // initialize maps of agents by disease group and distance
+        Map<DiseaseGroup, Map<Integer, Integer>> consByDiseaseGroupAtGeodesicDistance =
+                new HashMap<DiseaseGroup, Map<Integer, Integer>>();
+
+        for (DiseaseGroup dg : DiseaseGroup.values()) {
+            consByDiseaseGroupAtGeodesicDistance.put(dg, new HashMap<Integer, Integer>());
+        }
+
         Iterator<Agent> it = agent.getNetwork().getAgentIterator();
         while (it.hasNext()) {
             Agent otherAgent = it.next();
 
             // geodesic distance to other agent
-            Integer gdd = null;
-            if ((with != null) && (otherAgent.getId() == with.getId())) {
-                gdd = 1;
-            } else if ((without != null) && (otherAgent.getId() == without.getId())) {
-                // gd = null;
-            } else {
-                gdd = agent.getGeodesicDistanceTo(otherAgent);
-            }
+            Integer gdd = agent.getGeodesicDistanceTo(otherAgent);
 
             // if geodesic connection exists
             if (gdd != null) {
@@ -331,73 +389,165 @@ public final class StatsComputer {
                     default:
                         logger.warn("Unhandled disease group: " + otherAgent.getDiseaseGroup());
                 }
-
-                // direct connections
-                if (gdd == 1) {
-                    // store for triad checks
-                    directConnections.add(otherAgent);
-
-                    // geographic distance to direct connections
-                    double ggd = agent.getGeographicDistanceTo(otherAgent);
-                    // store disease group and geographic distance
-                    switch (otherAgent.getDiseaseGroup()) {
-                        case SUSCEPTIBLE:
-                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).put(ggd,
-                                    directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).get(ggd) != null ?
-                                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.SUSCEPTIBLE).get(ggd) + 1 : 1);
-                            break;
-
-                        case INFECTED:
-                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).put(ggd,
-                                    directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).get(ggd) != null ?
-                                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.INFECTED).get(ggd) + 1 : 1);
-                            break;
-
-                        case RECOVERED:
-                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).put(ggd,
-                                    directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).get(ggd) != null ?
-                                            directConsByDiseaseGroupAtGeographicDistance.get(DiseaseGroup.RECOVERED).get(ggd) + 1 : 1);
-                            break;
-
-                        default:
-                            logger.warn("Unhandled disease group: " + otherAgent.getDiseaseGroup());
-                    }
-                }
             }
-
         }
 
-        // determine number of triads agent belongs to
-        int n = agent.getNetwork().getN();
+        return consByDiseaseGroupAtGeodesicDistance;
+    }
+
+//    /**
+//     * Gets the number of direct connections by disease group and distance.
+//     *
+//     * @param dg
+//     *          the disease group
+//     * @param dist
+//     *          the distance
+//     * @return the number of direct connections by disease group and distance
+//     */
+//    public static int getConnectionsByDiseaseGroupAndGeodesicDistance(DiseaseGroup dg, Integer dist) {
+//        Map<Integer, Integer> consByDiseaseGroup = computeConnectionsByDiseaseGroupAtGeodesicDistance().get(dg);
+//        if (consByDiseaseGroup != null) {
+//            Integer consByDistance = consByDiseaseGroup.get(dist);
+//            if (consByDistance != null) {
+//                return consByDistance;
+//            }
+//        }
+//        return 0;
+//    }
+
+    /**
+     * Gets the number of connections by geodesic distance (irrespective of disease group):
+     * map<distance, number>.
+     *
+     * @param agent
+     *          the agent to get the geodesic distances for
+     * @return the number of direct connections by disease group and distance
+     */
+    public static Map<Integer, Integer> getConnectionsByGeodesicDistance(Agent agent) {
+
+        Map<Integer, Integer> consByDist = new HashMap<Integer, Integer>();
+
+        for (DiseaseGroup dg : DiseaseGroup.values()) {
+             for (Map.Entry<Integer, Integer> entry : computeConnectionsByDiseaseGroupAtGeodesicDistance(agent).get(dg).entrySet()) {
+                 Integer dist = entry.getKey();
+                 Integer cons = entry.getValue();
+                 consByDist.put(dist,
+                         consByDist.get(dist) != null ?
+                                 consByDist.get(dist) + cons :
+                                     cons);
+             }
+        }
+        return consByDist;
+    }
+
+    /**
+     * Computes the stats for a single agent's connections with a potentially new connection and without an existing connection.
+     *
+     * @param agent
+     *          the agent
+     * @param with
+     *          the potentially new connection
+     * @param without
+     *          the existing connection not to consider
+     * @return the stats for a single agent's connections.
+     */
+    public static LocalAgentConnectionsStats computeLocalAgentConnectionsStats(Agent agent, Agent with, Agent without) {
+
+        // direct ties
+        int n = 0;
+        int nS = 0;
+        int nI = 0;
+        int nR = 0;
+        // ties at distance 2
+        int m = 0;
+        int mS = 0;
+        int mI = 0;
+        int mR = 0;
         // open triads
-        int yGlobal = (n*(n-1))/2;
+        int yGlobal = 0;
         int yLocal = 0;
         // closed triads
         int z = 0;
-        it = directConnections.iterator();
-        // consider direct connections only once for triads
-        List<Agent> consideredDirectConnections = new LinkedList<Agent>(directConnections);
-        while (it.hasNext()) {
-            Agent directConnection = it.next();
-            consideredDirectConnections.remove(directConnection);
-            Iterator<Agent> cit = consideredDirectConnections.iterator();
-            while (cit.hasNext()) {
-                Agent potentialTriad = cit.next();
-                if (directConnection.isDirectlyConnectedTo(potentialTriad)) {
-                    z++;
-                } else {
-                    yLocal++;
+        // network size
+        int netSize = agent.getNetwork().getN();
+
+        // preparing the list of direct connections
+        Collection<Agent> directConnections = agent.getConnections();
+        if (with != null) {
+            directConnections.add(with);
+        }
+        if (without != null && !directConnections.isEmpty() && directConnections.contains(without)) {
+            directConnections.remove(without);
+        }
+
+        if (!directConnections.isEmpty()) {
+
+            n = directConnections.size();
+            yGlobal = (n*(n-1))/2;
+
+            Iterator<Agent> it = directConnections.iterator();
+            List<Agent> consideredAtDistance2 = new LinkedList<Agent>();
+            List<Agent> consideredDirectConnections = new LinkedList<Agent>(directConnections);
+            while (it.hasNext()) {
+                Agent directConnection = it.next();
+
+                // direct connections by disease state
+                switch(directConnection.getDiseaseGroup()) {
+                    case SUSCEPTIBLE:
+                        nS++;
+                        break;
+                    case INFECTED:
+                        nI++;
+                        break;
+                    case RECOVERED:
+                        nR++;
+                        break;
+                    default:
+                        logger.warn("Unknown disease state: " + directConnection.getDiseaseGroup());
+                }
+
+                // connections at distance 2
+                Collection<Agent> connectionsAtDistance2 = directConnection.getConnections();
+                Iterator<Agent> cDist2It = connectionsAtDistance2.iterator();
+                while (cDist2It.hasNext()) {
+                    Agent connectionAtDistance2 = cDist2It.next();
+                    if (consideredAtDistance2.contains(connectionAtDistance2) ||
+                            directConnections.contains(connectionAtDistance2) ||
+                            agent.equals(connectionAtDistance2)) {
+                        continue;
+                    }
+                    m++;
+                    switch(connectionAtDistance2.getDiseaseGroup()) {
+                        case SUSCEPTIBLE:
+                            mS++;
+                            break;
+                        case INFECTED:
+                            mI++;
+                            break;
+                        case RECOVERED:
+                            mR++;
+                            break;
+                        default:
+                            logger.warn("Unknown disease state: " + directConnection.getDiseaseGroup());
+                    }
+                    consideredAtDistance2.add(connectionAtDistance2);
+                }
+
+                // counting open and closed triads
+                consideredDirectConnections.remove(directConnection);
+                Iterator<Agent> cit = consideredDirectConnections.iterator();
+                while (cit.hasNext()) {
+                    Agent potentialTriad = cit.next();
+                    if (directConnection.isDirectlyConnectedTo(potentialTriad)) {
+                        z++;
+                    } else {
+                        yLocal++;
+                    }
                 }
             }
         }
 
-        return new LocalAgentConnectionsStats(
-                consByDiseaseGroupAtGeodesicDistance,
-                directConsByDiseaseGroupAtGeographicDistance,
-                yGlobal,
-                yLocal,
-                z,
-                n);
+        return new LocalAgentConnectionsStats(n, nS, nI, nR, m, mS, mI, mR, yGlobal, yLocal, z, netSize);
     }
 
     /**
